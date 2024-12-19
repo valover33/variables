9864aae62251c648a819c17c1ba2f54608201b63
valover33 <83272190+valover33@users.noreply.github.com>
2024-12-19 21:28:49 +0300

    Add files via upload

---
+++ variables.py
@@ -0,0 +1,8 @@
+# Практическое задание по теме "Переменные"
+
+number_of_homeworks = 12
+number_of_time = 1.5
+сourse_name = "Python"
+time_for_one_task = number_of_time / number_of_homeworks
+print('Курс:', сourse_name + ',', 'всего задач:', str(number_of_homeworks) + ',', 'затрачено часов:', str(number_of_time) + ',', \
+                                'среднее время выполнения', time_for_one_task, 'часа')
